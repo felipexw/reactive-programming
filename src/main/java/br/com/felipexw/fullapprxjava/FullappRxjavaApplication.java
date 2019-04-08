3e93737ea77dad20b57ce4d212f167df905d92fe
Felipe Appio <felipe.appio@senior.com.br>
2019-04-08 11:25:49 -0300

    Inciado estrutura do projeto

---
+++ src/main/java/br/com/felipexw/fullapprxjava/FullappRxjavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipexw.fullapprxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullappRxjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullappRxjavaApplication.class, args);
+	}
+
+}
